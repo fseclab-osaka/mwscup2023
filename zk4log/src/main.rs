ffc191513616d5b6e509ab035b771ae99d4626c7
Takumi Matsuura <tm99hjkl@gmail.com>
2023-09-18 11:58:27 +0900

    Fix build failure

--- zk4log/src/main.rs
+++ zk4log/src/main.rs
@@ -1,6 +1,6 @@
 use nu_plugin::{serve_plugin, MsgPackSerializer};
-user nu_plugin_zk4log::Zk4log;
+use nu_plugin_zk4log::Zk4log;
 
 fn main() {
-    serve_pugin(&mut Zk4log {}, MsgPackSerializer {})
+    serve_plugin(&mut Zk4log {}, MsgPackSerializer {})
 }
