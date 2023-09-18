081ebb9c4407f686ee3e4652f76770e24e572067
Takumi Matsuura <tm99hjkl@gmail.com>
2023-09-18 11:54:09 +0900

    Fix build failure

--- zk4log/src/nu/mod.rs
+++ zk4log/src/nu/mod.rs
@@ -4,13 +4,11 @@ use nu_protocol::{PluginSignature, SyntaxShape, Value};
 
 impl Plugin for Zk4log {
     fn signature(&self) -> Vec<PluginSignature> {
-        vec![
-            PluginSignature::build("zk4log")
-                .usage("zk4log")
-                .required("mode", SyntaxShape::String, "mode") // hide, verify, or open
-                .named("json", SyntaxShape::String, "json file name", Some('i'))
-                .named("proof", SyntaxShape::String, "proof file name", Some('p'))
-        ]
+        vec![PluginSignature::build("zk4log")
+            .usage("zk4log")
+            .required("mode", SyntaxShape::String, "mode") // hide, verify, or open
+            .named("json", SyntaxShape::String, "json file name", Some('i'))
+            .named("proof", SyntaxShape::String, "proof file name", Some('p'))]
     }
 
     fn run(
@@ -20,7 +18,8 @@ impl Plugin for Zk4log {
         input: &Value,
     ) -> Result<Value, LabeledError> {
         match name {
-            // 
+            //
+            _ => todo!(),
         }
     }
 }
