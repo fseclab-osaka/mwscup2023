795af3fbc2ecd4e1237142c2f5604d5cdc3a4ead
y-takeuchi25 <127864765+y-takeuchi25@users.noreply.github.com>
2023-09-28 11:32:14 +0900

    modify help

--- zk4log/src/nu/mod.rs
+++ zk4log/src/nu/mod.rs
@@ -6,16 +6,16 @@ impl Plugin for Zk4log {
     fn signature(&self) -> Vec<PluginSignature> {
         vec![
             PluginSignature::build("zk4log hide")
-                .usage("zk4log hide -h")
-                .required("path", SyntaxShape::String, ""),
+                .usage("Hide log and generate proof")
+                .required("path", SyntaxShape::String, "json file path"),
             PluginSignature::build("zk4log verify")
-                .usage("zk4log verify -h")
-                .required_named("json", SyntaxShape::String, "json file name", Some('i'))
+                .usage("Verify secret log with proof")
+                .required_named("json", SyntaxShape::String, "json file name", Some('j'))
                 .required_named("proof", SyntaxShape::String, "proof file name", Some('p'))
                 .required_named("key", SyntaxShape::String, "key file name", Some('k')),
             PluginSignature::build("zk4log open")
-                .usage("zk4log open -h")
-                .required("path", SyntaxShape::String, ""),
+                .usage("Display log json file")
+                .required("path", SyntaxShape::String, "json file path"),
             PluginSignature::build("zk4log")
                 .usage("Log Analysis Tool with ZKP")
                 .required(
